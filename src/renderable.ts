3c8ff64ba08591b30395215269a90ad57df3012d
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-01-31 18:28:47 +0100

    changed defult shape to have middle where position is

--- src/renderable.ts
+++ src/renderable.ts
@@ -3,8 +3,8 @@ export default class Renderable extends Component {
     constructor(public position: Point, public color: string = "#000", public drawSelf = (ctx) => {
 
         ctx.fillStyle = '#cc0000';   
-        ctx.fillRect(this.position.x, this.position.y, 3, 3);
-        
+        ctx.fillRect(this.position.x - 1, this.position.y - 1, 2, 2);
+
     }) {
         super();
     }
