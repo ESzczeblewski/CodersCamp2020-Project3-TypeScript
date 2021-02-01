145366136988f2fd4bbebaf15cbb32d171349372
AnnZak <zak.anna.94@gmail.com>
2021-02-01 19:10:00 +0100

    first version

--- src/physicsEngine.ts
+++ src/physicsEngine.ts
@@ -1,6 +1,7 @@
 export default class PhysicsEngine {
 
     constructor(private readonly canvas: HTMLCanvasElement) {
+
         this.ctx = this.canvas.getContext('2d');
         this.width = this.ctx.canvas.width;
         this.height = this.ctx.canvas.height;
@@ -16,15 +17,7 @@ export default class PhysicsEngine {
             entity.component[1].velocity.x = randomXV(entity.component[1].position.x);
             entity.component[1].velocity.y = random(1,2);
 
-
-
-            this.spped = entity.component[1].speed;
-            this.visible = entity.component[1].visible;
-            this.color = entity.component[1].color;
-            
-            this.velocity.x *= 0.99;
-            this.velocity.y += GRAVITY;
-         
+            entity.component[1].velocity.x *= 0.99; // air resistance
         }
     }
     
@@ -32,7 +25,7 @@ export default class PhysicsEngine {
         if (x > this.width / 2) {
             return Math.random(-1.5, 0);
         } else {
-            return Math.random(0, 1.5)
+            return Math.random(0, 1.5);
         }
     }
 }
