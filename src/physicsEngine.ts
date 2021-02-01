2a46e2a96c4ccd450ace2a15b10df470330a8c3c
AnnZak <zak.anna.94@gmail.com>
2021-02-01 18:30:14 +0100

    first version

---
+++ src/physicsEngine.ts
@@ -0,0 +1,11 @@
+export default class PhysicsEngine {
+
+
+    constructor(public UpdatePosition){
+
+    }
+}
+
+
+/*Update position ma przeszukiwać arrey entity i przechodzić z jednego stanu do drugiego. Bazując na components
+publiczny ma też być arrey z nachodzącymi an siebie bommbelkami*/
