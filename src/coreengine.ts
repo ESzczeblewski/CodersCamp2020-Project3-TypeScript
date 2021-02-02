075ab7c64b23e55c5418d321c1746ff0a4956eae
Cidebur <mcibor96@gmail.com>
2021-02-02 17:26:02 +0100

    core v0.2

--- src/coreengine.ts
+++ src/coreengine.ts
@@ -8,12 +8,17 @@ export class CoreEngine {
     private _cursorPosition: Point = {x: 0, y: 0};
     private _prevCursorPosition: Point = {x: 0, y: 0};
     private _entities: Array<Entity> =[];
-    // private _audioEngine: AudioEngine = new AudioEngine();
-    // private _renderEngine = new RenderEngine();
-    // private _uiEngine = new UIEngine();
+    private _ctx: CanvasRenderingContext2D | null;
+    // private _renderEngine;
+    // private _uiEngine;
     // private _physicsEngine = new PhysicsEngine();
+    // private _audioEngine: AudioEngine = new AudioEngine();
 
-    constructor(private _canvas: HTMLCanvasElement, private _controller: IInputDevice) { }
+    constructor(private _canvas: HTMLCanvasElement, private _controller: IInputDevice) { 
+        this._ctx = this._canvas.getContext("2d");
+        //this._renderEngine = new RenderEngine(this._canvas, this._ctx);
+        //this._uiEngine = new UIEngine(this._canvas);
+    }
 
     public get cursorPosition() {
         return this._cursorPosition;
@@ -31,11 +36,22 @@ export class CoreEngine {
 
     // }
 
-    public mainLoop(callback = () => {}) {
-        
-        //add engine components functionality here...
+    public init(callback = () => {}) {
+        window.requestAnimationFrame(() => this._mainLoop(callback))
+    }
 
-        callback();
+    // public 
+
+    private _mainLoop(callback: () => void) {
+    
+        
+            // //add engine components functionality here...
+            
+            // this._physicsEngine.updatePosition(this._entities.map(entity => entity.hasComponent(Colidable)));
+            // this._renderEngine.render(this._entities.map(entity => entity.hasComponent(Renderable)));
+            this._readInput();
+
+            callback();
     }
 
     private _readInput(): void {
