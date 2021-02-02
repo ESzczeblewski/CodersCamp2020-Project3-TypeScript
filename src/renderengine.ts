64ee8382b5f102fbadfcc489302afc9013cf0aad
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-02-02 16:59:50 +0100

    renderengine receives context as parameter

--- src/renderengine.ts
+++ src/renderengine.ts
@@ -2,20 +2,18 @@ import Renderable from "./renderable";
 import { Entity } from "./entity"
 
 export default class RenderEngine {
-    public ctx;
 
-    constructor(public readonly canvas: HTMLCanvasElement) {
-        this.ctx = this.canvas.getContext('2d')!;
+    constructor(private _canvas: HTMLCanvasElement, private _ctx: CanvasRenderingContext2D) {
     }
 
     public render(entitiesArray: Array<Entity>): void {
 
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // czyści canvas
+        this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height); // czyści canvas
 
         for (const entity of entitiesArray) {
             try {
                 let component = entity.getComponent(Renderable);
-                component.drawSelf(this.ctx); // obiekty renderują się same (po wybraniu, czym chcą być)
+                component.drawSelf(this._ctx); // obiekty renderują się same (po wybraniu, czym chcą być)
             } catch (err) {
                 console.error(`Could not find component of class Renderable in entity: ${entity.constructor.name}`);
             }
