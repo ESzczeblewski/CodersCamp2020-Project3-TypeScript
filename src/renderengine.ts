65ad354250929fe2bad3129553a596fcd0367659
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-02-02 16:18:30 +0100

    fixed renderengine.ts

--- src/renderengine.ts
+++ src/renderengine.ts
@@ -1,3 +1,5 @@
+import Renderable from "./renderable";
+
 export default class RenderEngine {
     public ctx;
 
@@ -6,9 +8,18 @@ export default class RenderEngine {
     }
 
     public render(entitiesArray: Array<Entity>): void {
+
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); // czyści canvas
+
         for (const entity of entitiesArray) {
-            entity.components[0].drawSelf(this.ctx); // obiekty renderują się same (po wybraniu, czym chcą być)
+            try {
+                let component = entity.getComponent(Renderable);
+                component.drawSelf(this.ctx); // obiekty renderują się same (po wybraniu, czym chcą być)
+            } catch (err) {
+                console.error(`Could not find component of class Renderable in entity: ${entity.constructor.name}`);
+            }
         }
+
     }
+
 }
