7d6bb227a526a60eb5396645265dcfcc21941a26
Cidebur <mcibor96@gmail.com>
2021-02-05 20:12:42 +0100

    Add Engine/index.ts which exports GameEngine functionality

--- src/Engine/index.ts
+++ src/Engine/index.ts
@@ -1,3 +1,9 @@
+import Engine from "./CoreEngine/CoreEngine"
+import Entity from "./Entity/Entity"
+import Collidable from "./Components/Colidable"
+import Renderable from "./Components/Renderable"
+import Component from "./Components/Component"
 import { Vector } from "./Utils/vector.h"
+import { IPointerDevice } from "./Input/inputs.h"
 
-export { Vector }
+export { Engine, Entity, Component, Collidable, Renderable, Vector, IPointerDevice }
